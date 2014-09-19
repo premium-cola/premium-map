970f2a32b492511c4647720a745d5d4073fa42d8
Karolin Varner <karo@cupdev.net>
2014-09-19 12:41:00 +0200

    Remove tests
    For the moment this repo will be maintained in
    least-effort-mode as there is not very much to come.

--- test/unit/address_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class AddressTest < ActiveSupport::TestCase
-
-  def test_parse_file
-    Address.parse_excel(File.expand_path("../../data/example.xls", __FILE__))
-  end
-
-end
